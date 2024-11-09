fb43948ce2743b93a8cb3389c5a569da6abbfe06
valentin e <valentineremia@yahoo.com>
2024-11-09 19:50:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const questions = [
+    {
+        id: 1,
+        question: "Care sunt culorile drapelului României?",
+        options: ["Roșu", "Galben", "Albastru", "Verde"],
+        correctAnswers: [0, 1, 2] // Indexurile răspunsurilor corecte
+    },
+    {
+        id: 2,
+        question: "Care este capitala României?",
+        options: ["București", "Cluj-Napoca", "Iași", "Timișoara"],
+        correctAnswers: [0,] // Indexurile răspunsurilor corecte
+    },
+    {
+        id: 3,
+        question: "Care este cel mai înalt munte din România?",
+        options: ["Pădurea Neagră", "Muntele Bucegi", "Muntele Retezat", "Muntele Moldoveanu"],
+        correctAnswers: [2] // Indexurile răspunsurilor corecte
+    },
+    {
+        id: 4,
+        question: "Care este cel mai mare lac din România?",
+        options: ["Lacul Vidraru", "Lacul Roșu", "Lacul Izvorul Muntelui", "Lacul Sf. Ana"],
+        correctAnswers: [0] // Indexurile răspunsurilor corecte
+    },
+    {
+        id: 5,
+        question: "Care este cel mai mare oraș din România?",
+        options: ["București", "Cluj-Napoca", "Iași", "Timișoara"],
+        correctAnswers: [0] // Indexurile răspunsurilor corecte
+    },
+
+    // Adaugă mai multe întrebări
+];
+
+app.get('/questions', (req, res) => {
+    res.json(questions);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
