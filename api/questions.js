8a9f79f3dfcc207809fecc55e0fa83247d9371d1
valentin e <valentineremia@yahoo.com>
2024-11-09 20:33:35 +0200

    #new

--- api/questions.js
+++ api/questions.js
@@ -6,36 +6,35 @@ const questions = [
         id: 1,
         question: "Care sunt culorile drapelului României?",
         options: ["Roșu", "Galben", "Albastru", "Verde"],
-        correctAnswers: [0, 1, 2] // Indexurile răspunsurilor corecte
+        correctAnswers: [0, 1, 2]
     },
     {
         id: 2,
         question: "Care este capitala României?",
         options: ["București", "Cluj-Napoca", "Iași", "Timișoara"],
-        correctAnswers: [0] // Indexurile răspunsurilor corecte
+        correctAnswers: [0]
     },
     {
         id: 3,
         question: "Care este cel mai înalt munte din România?",
         options: ["Pădurea Neagră", "Muntele Bucegi", "Muntele Retezat", "Muntele Moldoveanu"],
-        correctAnswers: [3] // Indexurile răspunsurilor corecte
+        correctAnswers: [3]
     },
     {
         id: 4,
         question: "Care este cel mai mare lac din România?",
         options: ["Lacul Vidraru", "Lacul Roșu", "Lacul Izvorul Muntelui", "Lacul Sf. Ana"],
-        correctAnswers: [2] // Indexurile răspunsurilor corecte
+        correctAnswers: [2]
     },
     {
         id: 5,
         question: "Care este cel mai mare oraș din România?",
         options: ["București", "Cluj-Napoca", "Iași", "Timișoara"],
-        correctAnswers: [0] // Indexurile răspunsurilor corecte
-    },
-    // Adaugă mai multe întrebări
+        correctAnswers: [0]
+    }
 ];
 
-app.get('/questions', (req, res) => {
+app.get('/', (req, res) => {
     res.json(questions);
 });
 
